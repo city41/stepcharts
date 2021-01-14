a93866686b8aafa978b4d41f1b9c4fea28cbf619
Matt Greer <matt.e.greer@gmail.com>
2021-01-13 21:02:51 -0500

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import Nav from '../components/nav'
+import React from "react";
 
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
       <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+        <h1 className="text-2xl text-center text-gray-700 dark:text-gray-100">
           Next.js + Tailwind CSS 2.0
         </h1>
       </div>
     </div>
-  )
+  );
 }
