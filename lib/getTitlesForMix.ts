faf47f9c51c45e35279f47187cbb27323c0d461d
Matt Greer <matt.e.greer@gmail.com>
2021-01-15 15:39:41 -0500

    getTitlesForMix: get title from the chart not directory

--- lib/getTitlesForMix.ts
+++ lib/getTitlesForMix.ts
@@ -1,11 +1,15 @@
 import fs from "fs";
 import path from "path";
+import {parseStepchart} from "./parseStepchart";
 
 const ROOT = "stepcharts";
 
-function getTitlesForMix(mix: string): string[] {
-  const titleDirs = fs.readdirSync(path.join(ROOT, mix));
-  return titleDirs;
+function getTitlesForMix(mixDir: string): string[] {
+  const titleDirs = fs.readdirSync(path.join(ROOT, mixDir));
+  return titleDirs.map(titleDir => {
+    const stepchart = parseStepchart(path.join(ROOT, mixDir, titleDir);
+    return stepchart.title;
+  });
 }
 
 export { getTitlesForMix };
