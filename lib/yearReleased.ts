172eeb1ba43ef480a0ca1b68107788d70bba751b
Matt Greer <matt.e.greer@gmail.com>
2021-02-03 23:20:54 -0500

    Dancing Stage mixes

--- lib/yearReleased.ts
+++ lib/yearReleased.ts
@@ -8,6 +8,12 @@ const yearReleased: Record<string, number> = {
   X: 2008,
   X2: 2010,
   "X3 vs 2nd Mix": 2011,
+  "Dancing Stage 1.5": 1999,
+  "Dancing Stage EuroMIX": 2000,
+  "Dancing Stage EuroMIX2": 2002,
+  "Dancing Stage featuring Disney Rave": 2000,
+  "Dancing Stage Fusion": 2005,
+  "Dancing Stage featuring Dreams Come True": 1999,
 };
 
 export { yearReleased };
