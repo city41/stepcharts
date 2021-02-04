b6910f36939b335824609ed9cd29cf942b0a3709
Matt Greer <matt.e.greer@gmail.com>
2021-02-03 21:58:30 -0500

    IndexPage: content-center

--- components/IndexPage.tsx
+++ components/IndexPage.tsx
@@ -25,7 +25,7 @@ function IndexPage({ mixes }: IndexPageProps) {
       metaDescription=""
       socialMediaImg=""
     >
-      <ul className="mt-8 sm:mt-16 flex flex-col sm:flex-row sm:flex-wrap items-center sm:items-start sm:pl-4 lg:pl-0">
+      <ul className="mt-8 sm:mt-16 flex flex-col sm:flex-row sm:flex-wrap content-center items-center sm:items-start sm:pl-4 lg:pl-0">
         {mixes.map((m) => {
           const mixBannerUrl = require(`../prodStepcharts/${m.mixDir}/mix-banner.png`);
           return (
