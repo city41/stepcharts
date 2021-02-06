08d02c12f8332b3e6519942a4df4ecabcea6e425
Matt Greer <matt.e.greer@gmail.com>
2021-02-06 11:19:43 -0500

    BrandNewModal

--- components/layout/Root.tsx
+++ components/layout/Root.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import { Head } from "./Head";
 import { Footer } from "./Footer";
+import { BrandNewModal } from "../BrandNewModal";
 
 import logoSvg from "./logoArrow.svg";
 
@@ -23,41 +24,44 @@ function Root({
   children,
 }: RootProps) {
   return (
-    <div
-      className={clsx(
-        styles.background,
-        "flex flex-col items-start items-stretch"
-      )}
-    >
-      <Head
-        title={title}
-        metaDescription={metaDescription}
-        metaImg={socialMediaImg}
-      />
-      <header>
-        <div className="bg-heading h-16 border-b-4 border-heading-border flex flex-row items-center justify-start lg:px-4">
-          <a href="/" className={clsx(styles.logo, "block lg:-ml-4")}>
-            <img className="w-full h-full" src={logoSvg} />
-          </a>
-          <div className="text-2xl font-bold text-focal text-center pl-4">
-            Stepcharts
-          </div>
-        </div>
-        {subtitle && (
-          <div className="hidden sm:block py-1 bg-subheading border-b-2 border-heading-border">
-            <div className="w-full max-w-6xl mx-auto flex flex-row items-center">
-              <h1 className="font-bold text-white text-center sm:text-left">
-                {subtitle}
-              </h1>
+    <>
+      <BrandNewModal />
+      <div
+        className={clsx(
+          styles.background,
+          "flex flex-col items-start items-stretch"
+        )}
+      >
+        <Head
+          title={title}
+          metaDescription={metaDescription}
+          metaImg={socialMediaImg}
+        />
+        <header>
+          <div className="bg-heading h-16 border-b-4 border-heading-border flex flex-row items-center justify-start lg:px-4">
+            <a href="/" className={clsx(styles.logo, "block lg:-ml-4")}>
+              <img className="w-full h-full" src={logoSvg} />
+            </a>
+            <div className="text-2xl font-bold text-focal text-center pl-4">
+              Stepcharts
             </div>
           </div>
-        )}
-      </header>
-      <main role="main" className="flex-1 max-w-6xl w-full mx-auto">
-        {children}
-      </main>
-      <Footer className="mt-16" />
-    </div>
+          {subtitle && (
+            <div className="hidden sm:block py-1 bg-subheading border-b-2 border-heading-border">
+              <div className="w-full max-w-6xl mx-auto flex flex-row items-center">
+                <h1 className="font-bold text-white text-center sm:text-left">
+                  {subtitle}
+                </h1>
+              </div>
+            </div>
+          )}
+        </header>
+        <main role="main" className="flex-1 max-w-6xl w-full mx-auto">
+          {children}
+        </main>
+        <Footer className="mt-16" />
+      </div>
+    </>
   );
 }
 
